5fdb16d63911fbdd7ee08efc406ab3301f086df8
vedantchimote <vedantc.code@gmail.com>
2024-07-27 18:27:27 +0530

    ✨ Added UserPdfExporter class to export users to PDF format. Closes #30

--- curiositees-backend/curiositees/CuriositeesWebParent/CuriositeesBackend/src/main/java/com/sunbeaminfo/curiositees/admin/user/UserController.java
+++ curiositees-backend/curiositees/CuriositeesWebParent/CuriositeesBackend/src/main/java/com/sunbeaminfo/curiositees/admin/user/UserController.java
@@ -223,4 +223,12 @@ public class UserController {
     UserExcelExporter exporter = new UserExcelExporter();
     exporter.export(listUsers, response);
   }
+
+  @GetMapping("/users/export/pdf")
+  public void exportToPDF(HttpServletResponse response) throws IOException {
+    List<User> listUsers = userService.listAll();
+
+    UserPdfExporter exporter = new UserPdfExporter();
+    exporter.export(listUsers, response);
+  }
 }
