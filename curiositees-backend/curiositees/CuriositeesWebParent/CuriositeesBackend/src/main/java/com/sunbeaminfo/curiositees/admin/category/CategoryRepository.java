37a43fed58ed676b43522e2fa05f6e16f2c1c31d
vedantchimote <vedantc.code@gmail.com>
2024-08-06 01:13:20 +0530

    :sparkles: Added Category Entity and CategoryRepository. Closes #43

---
+++ curiositees-backend/curiositees/CuriositeesWebParent/CuriositeesBackend/src/main/java/com/sunbeaminfo/curiositees/admin/category/CategoryRepository.java
@@ -0,0 +1,21 @@
+/**
+ * @author : Vedant Chimote
+ * @mailto : vedantc.code@gmail.com
+ * @created : 06-08-2024, Tuesday
+ * @Time : 01:07 am
+ **/
+
+package com.sunbeaminfo.curiositees.admin.category;
+
+import com.curiositees.common.entity.Category;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+/**
+ * @author : Vedant Chimote
+ * @mailto : vedantc.code@gmail.com
+ * @created : 06-08-2024, Tuesday
+ **/
+
+public interface CategoryRepository extends PagingAndSortingRepository<Category, Integer> {
+
+}
