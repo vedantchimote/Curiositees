ea4678ff76e44d6d51ea07c2cb21490462e0ddac
vedantchimote <vedantc.code@gmail.com>
2024-08-13 01:21:13 +0530

    Added extra images on product form. Closes #84

--- curiositees-backend/curiositees/CuriositeesWebParent/CuriositeesBackend/src/main/resources/static/js/common_form.js
+++ curiositees-backend/curiositees/CuriositeesWebParent/CuriositeesBackend/src/main/resources/static/js/common_form.js
@@ -5,14 +5,8 @@ $(document).ready(function () {
 
   $("#fileImage").change(function () {
 
-    fileSize = this.files[0].size;
-
-    if (fileSize > MAX_FILE_SIZE) {
-      this.setCustomValidity("You must choose an image less than " + MAX_FILE_SIZE + " bytes!");
-      this.reportValidity();
-    } else {
-      this.setCustomValidity("");
-      showImageThumbnail(this);
+    if (!checkFileSize(this)) {
+      return;
     }
   });
 });
@@ -24,6 +18,21 @@ function showImageThumbnail(fileInput) {
     $("#thumbnail").attr("src", e.target.result);
   };
   reader.readAsDataURL(file);
+}
+
+function checkFileSize(fileInput) {
+  fileSize = fileInput.files[0].size;
+
+  if (fileSize > MAX_FILE_SIZE) {
+    fileInput.setCustomValidity("You must choose an image less than " + MAX_FILE_SIZE + " bytes!");
+    fileInput.reportValidity();
+
+    return false;
+  } else {
+    fileInput.setCustomValidity("");
+
+    return true;
+  }
 }
 
 function showModalDialog(title, message) {
