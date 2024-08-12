f7bf47c859a6edb99b2a4ba6906fe44b01936e67
vedantchimote <vedantc.code@gmail.com>
2024-08-13 01:41:06 +0530

    Coded upload product's extra images. Closes #85

--- curiositees-backend/curiositees/CuriositeesWebParent/CuriositeesBackend/src/main/java/com/sunbeaminfo/curiositees/admin/product/ProductController.java
+++ curiositees-backend/curiositees/CuriositeesWebParent/CuriositeesBackend/src/main/java/com/sunbeaminfo/curiositees/admin/product/ProductController.java
@@ -1,19 +1,12 @@
-/**
- * @author : Vedant Chimote
- * @mailto : vedantc.code@gmail.com
- * @created : 12-08-2024, Monday
- * @Time : 09:24 pm
- **/
-
 package com.sunbeaminfo.curiositees.admin.product;
 
 import com.curiositees.common.entity.Brand;
 import com.curiositees.common.entity.Product;
 import com.sunbeaminfo.curiositees.admin.FileUploadUtil;
 import com.sunbeaminfo.curiositees.admin.brand.BrandService;
-import jakarta.transaction.Transactional;
 import java.io.IOException;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -25,89 +18,123 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-/**
- * @author : Vedant Chimote
- * @mailto : vedantc.code@gmail.com
- * @created : 12-08-2024, Monday
- **/
 
 @Controller
 public class ProductController {
+	@Autowired private ProductService productService;
+	@Autowired private BrandService brandService;
+	
+	@GetMapping("/products")
+	public String listAll(Model model) {
+		List<Product> listProducts = productService.listAll();
+		
+		model.addAttribute("listProducts", listProducts);
+		
+		return "products/products";
+	}
+	
+	@GetMapping("/products/new")
+	public String newProduct(Model model) {
+		List<Brand> listBrands = brandService.listAll();
+		
+		Product product = new Product();
+		product.setEnabled(true);
+		product.setInStock(true);
+		
+		model.addAttribute("product", product);
+		model.addAttribute("listBrands", listBrands);
+		model.addAttribute("pageTitle", "Create New Product");
+		
+		return "products/product_form";
+	}
+	
+	@PostMapping("/products/save")
+	public String saveProduct(Product product, RedirectAttributes ra,
+			@RequestParam("fileImage") MultipartFile mainImageMultipart,			
+			@RequestParam("extraImage") MultipartFile[] extraImageMultiparts) 
+					throws IOException {
+		setMainImageName(mainImageMultipart, product);
+		setExtraImageNames(extraImageMultiparts, product);
+			
+		Product savedProduct = productService.save(product);
+		
+		saveUploadedImages(mainImageMultipart, extraImageMultiparts, savedProduct);
+		
+		ra.addFlashAttribute("message", "The product has been saved successfully.");
+		
+		return "redirect:/products";
+	}
+	
+	private void saveUploadedImages(MultipartFile mainImageMultipart, 
+			MultipartFile[] extraImageMultiparts, Product savedProduct) throws IOException {
+		if (!mainImageMultipart.isEmpty()) {
+			String fileName = StringUtils.cleanPath(mainImageMultipart.getOriginalFilename());
+			String uploadDir = "../product-images/" + savedProduct.getId();
+			
+			FileUploadUtil.cleanDir(uploadDir);
+			FileUploadUtil.saveFile(uploadDir, fileName, mainImageMultipart);		
+		}
+		
+		if (extraImageMultiparts.length > 0) {
+			String uploadDir = "../product-images/" + savedProduct.getId() + "/extras";
+			
+			for (MultipartFile multipartFile : extraImageMultiparts) {
+				if (multipartFile.isEmpty()) continue;
+				
+				String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+				FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+			}
+		}
+		
+	}
 
-  @Autowired private ProductService productService;
+	private void setExtraImageNames(MultipartFile[] extraImageMultiparts, Product product) {
+		if (extraImageMultiparts.length > 0) {
+			for (MultipartFile multipartFile : extraImageMultiparts) {
+				if (!multipartFile.isEmpty()) {
+					String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+					product.addExtraImage(fileName);
+				}
+			}
+		}
+	}
 
-  @Autowired private BrandService brandService;
-
-  @GetMapping("/products")
-  public String listAll(Model model) {
-    List<Product> listProducts = productService.listAll();
-
-    model.addAttribute("listProducts", listProducts);
-
-    return "products/products";
-  }
-
-  @GetMapping("/products/new")
-  public String newProduct(Model model) {
-    List<Brand> listBrands = brandService.listAll();
-
-    Product product = new Product();
-    product.setEnabled(true);
-    product.setInStock(true);
-
-    model.addAttribute("product", product);
-    model.addAttribute("listBrands", listBrands);
-    model.addAttribute("pageTitle", "Create New Product");
-
-    return "products/product_form";
-  }
-
-  @PostMapping("/products/save")
-  public String saveProduct(Product product, RedirectAttributes ra,
-      @RequestParam("fileImage") MultipartFile multipartFile) throws IOException {
-
-    if (!multipartFile.isEmpty()) {
-      String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
-      product.setMainImage(fileName);
-
-      Product savedProduct = productService.save(product);
-      String uploadDir = "../product-images/" + savedProduct.getId();
-
-      FileUploadUtil.cleanDir(uploadDir);
-      FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
-
-    } else {
-      productService.save(product);
-    }
-    ra.addFlashAttribute("message", "The product has been saved successfully.");
-
-    return "redirect:/products";
-  }
-
-  @GetMapping("/products/{id}/enabled/{status}")
-  public String updateCategoryEnabledStatus(@PathVariable("id") Integer id,
-      @PathVariable("status") boolean enabled, RedirectAttributes redirectAttributes) {
-    productService.updateProductEnabledStatus(id, enabled);
-    String status = enabled ? "enabled" : "disabled";
-    String message = "The Product ID " + id + " has been " + status;
-    redirectAttributes.addFlashAttribute("message", message);
-
-    return "redirect:/products";
-  }
-
-  @GetMapping("/products/delete/{id}")
-  public String deleteProduct(@PathVariable(name = "id") Integer id,
-      Model model,
-      RedirectAttributes redirectAttributes) {
-    try {
-      productService.delete(id);
-
-      redirectAttributes.addFlashAttribute("message",
-          "The product ID " + id + " has been deleted successfully");
-    } catch (ProductNotFoundException ex) {
-      redirectAttributes.addFlashAttribute("message", ex.getMessage());
-    }
-
-    return "redirect:/products";
-  }
+	private void setMainImageName(MultipartFile mainImageMultipart, Product product) {
+		if (!mainImageMultipart.isEmpty()) {
+			String fileName = StringUtils.cleanPath(mainImageMultipart.getOriginalFilename());
+			product.setMainImage(fileName);
+		}
+	}
+	
+	@GetMapping("/products/{id}/enabled/{status}")
+	public String updateCategoryEnabledStatus(@PathVariable("id") Integer id,
+			@PathVariable("status") boolean enabled, RedirectAttributes redirectAttributes) {
+		productService.updateProductEnabledStatus(id, enabled);
+		String status = enabled ? "enabled" : "disabled";
+		String message = "The Product ID " + id + " has been " + status;
+		redirectAttributes.addFlashAttribute("message", message);
+		
+		return "redirect:/products";
+	}
+	
+	@GetMapping("/products/delete/{id}")
+	public String deleteProduct(@PathVariable(name = "id") Integer id, 
+			Model model,
+			RedirectAttributes redirectAttributes) {
+		try {
+			productService.delete(id);
+			String productExtraImagesDir = "../product-images/" + id + "/extras";
+			String productImagesDir = "../product-images/" + id;
+			
+			FileUploadUtil.removeDir(productExtraImagesDir);
+			FileUploadUtil.removeDir(productImagesDir);
+			
+			redirectAttributes.addFlashAttribute("message", 
+					"The product ID " + id + " has been deleted successfully");
+		} catch (ProductNotFoundException ex) {
+			redirectAttributes.addFlashAttribute("message", ex.getMessage());
+		}
+		
+		return "redirect:/products";
+	}	
 }
