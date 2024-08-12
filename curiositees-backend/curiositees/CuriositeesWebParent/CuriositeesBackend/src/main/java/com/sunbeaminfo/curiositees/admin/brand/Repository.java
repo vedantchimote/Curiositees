e9d25eac5259d7994e462aaabb42489de676776e
vedantchimote <vedantc.code@gmail.com>
2024-08-12 20:26:24 +0530

    Added Brand Entity and Brand Repository. Closes #63

---
+++ curiositees-backend/curiositees/CuriositeesWebParent/CuriositeesBackend/src/main/java/com/sunbeaminfo/curiositees/admin/brand/Repository.java
@@ -0,0 +1,23 @@
+/**
+ * @author : Vedant Chimote
+ * @mailto : vedantc.code@gmail.com
+ * @created : 12-08-2024, Monday
+ * @Time : 08:24 pm
+ **/
+
+package com.sunbeaminfo.curiositees.admin.brand;
+
+import com.curiositees.common.entity.Brand;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+/**
+ * @author : Vedant Chimote
+ * @mailto : vedantc.code@gmail.com
+ * @created : 12-08-2024, Monday
+ **/
+
+public interface Repository extends PagingAndSortingRepository<Brand, Integer>,
+    JpaRepository<Brand, Integer> {
+
+}
