d60df56fd0b109fba0637b9d7fc27c247d07136b
vedantchimote <vedantc.code@gmail.com>
2024-08-13 00:53:17 +0530

    Added delete product function. Closes #81

---
+++ curiositees-backend/curiositees/CuriositeesWebParent/CuriositeesBackend/src/main/java/com/sunbeaminfo/curiositees/admin/product/ProductNotFoundException.java
@@ -0,0 +1,22 @@
+/**
+ * @author : Vedant Chimote
+ * @mailto : vedantc.code@gmail.com
+ * @created : 13-08-2024, Tuesday
+ * @Time : 12:49 am
+ **/
+
+package com.sunbeaminfo.curiositees.admin.product;
+
+/**
+ * @author : Vedant Chimote
+ * @mailto : vedantc.code@gmail.com
+ * @created : 13-08-2024, Tuesday
+ **/
+
+public class ProductNotFoundException extends Exception {
+
+  public ProductNotFoundException(String message) {
+    super(message);
+  }
+
+}
