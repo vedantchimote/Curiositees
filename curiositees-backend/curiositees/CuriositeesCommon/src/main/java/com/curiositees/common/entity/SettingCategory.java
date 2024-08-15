9d123741d50ee3ccb76991284dc65c0b256ee986
vedantchimote <vedantc.code@gmail.com>
2024-08-15 15:29:52 +0530

    Coded setting entity, repository and unit tests. Closes #105

---
+++ curiositees-backend/curiositees/CuriositeesCommon/src/main/java/com/curiositees/common/entity/SettingCategory.java
@@ -0,0 +1,18 @@
+/**
+ * @author : Vedant Chimote
+ * @mailto : vedantc.code@gmail.com
+ * @created : 15-08-2024, Thursday
+ * @Time : 03:26 pm
+ **/
+
+package com.curiositees.common.entity;
+
+/**
+ * @author : Vedant Chimote
+ * @mailto : vedantc.code@gmail.com
+ * @created : 15-08-2024, Thursday
+ **/
+
+public enum SettingCategory {
+  GENERAL, MAIL_SERVER, MAIL_TEMPLATES, CURRENCY, PAYMENT
+}
---
+++ curiositees-backend/curiositees/CuriositeesWebParent/CuriositeesBackend/src/main/java/com/sunbeaminfo/curiositees/admin/setting/SettingRepository.java
@@ -0,0 +1,21 @@
+/**
+ * @author : Vedant Chimote
+ * @mailto : vedantc.code@gmail.com
+ * @created : 15-08-2024, Thursday
+ * @Time : 03:27 pm
+ **/
+
+package com.sunbeaminfo.curiositees.admin.setting;
+
+import com.curiositees.common.entity.Setting;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * @author : Vedant Chimote
+ * @mailto : vedantc.code@gmail.com
+ * @created : 15-08-2024, Thursday
+ **/
+
+public interface SettingRepository extends CrudRepository<Setting, String> {
+
+}
